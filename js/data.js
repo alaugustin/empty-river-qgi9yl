3e399a12e2d2e89ce8666db21568520ce59d94e5
aaug <aaug@caasco.ca>
2022-05-09 15:21:49 -0400

    extension change

---
+++ js/data.js
@@ -0,0 +1,26 @@
+export const cardData = [
+  {
+    "cardName": "À la carte",
+    "cardValue": 11,
+    "cardImg": "https://dummyimage.com/600x400/ff0000/fff",
+    "cardLink": "1-877-475-5061"
+  },
+  {
+    "cardName": "Basic",
+    "cardValue": 22,
+    "cardImg": "https://www.caasco.com/~/media/caasco/membership/cards/membership_card_Basic.png",
+    "cardLink": "https://www.caasco.com/membership/Join?Type=Basic"
+  },
+  {
+    "cardName": "Plus",
+    "cardValue": 111,
+    "cardImg": "https://www.caasco.com/~/media/caasco/membership/cards/membership_card_Plus.png",
+    "cardLink": "https://www.caasco.com/membership/Join?Type=Plus"
+  },
+  {
+    "cardName": "Premier",
+    "cardValue": 222,
+    "cardImg": "https://www.caasco.com/~/media/caasco/membership/cards/membership_card_Premier.png",
+    "cardLink": "https://www.caasco.com/membership/Join?Type=Premier"
+  }
+]
