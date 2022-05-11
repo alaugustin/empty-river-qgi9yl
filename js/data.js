8b20e09ed275b5d5f225eacae15e5bb38320a4de
aaug <aaug@caasco.ca>
2022-05-11 09:42:13 -0400

    data update

--- js/data.js
+++ js/data.js
@@ -1,26 +1,99 @@
-export const cardData = [
+let cardData = [
   {
     "cardName": "À la carte",
     "cardValue": 11,
     "cardImg": "https://dummyimage.com/600x400/ff0000/fff",
-    "cardLink": "1-877-475-5061"
+    "cardLink": "1-877-475-5061",
+    "roadside": [],
+    "savings": [
+      "Member pricing on Canadian passport photos",
+      "Member pricing on merchandise online with shopwithcaa.com and in CAA Stores⁴",
+      "Save instantly at any of the 124,000 participating retail locations in North America. You can save on apparel, restaurants, the latest tech, entertainment and more",
+      "Save or get perks on travel, including car rentals, hotel stays, vacation packages and more"
+    ],
+    "benefits": [
+      "Unlimited general practitioner consults per year at a 20% discounted rate with Maple","","",""
+    ]
   },
   {
     "cardName": "Basic",
     "cardValue": 22,
     "cardImg": "https://www.caasco.com/~/media/caasco/membership/cards/membership_card_Basic.png",
-    "cardLink": "https://www.caasco.com/membership/Join?Type=Basic"
+    "cardLink": "https://www.caasco.com/membership/Join?Type=Basic",
+    "roadside": [
+      "Four service calls",
+      "Towing distance of 10 km per call¹",
+      "Free emergency fuel delivery.Fuel costs extra.",
+      "Mobile battery service²",
+      "Member pricing on CAA Premium Battery purchase²",
+      "CAA Bike Assist™³",
+      "Car and bike coverage"
+    ],
+    "savings": [
+      "Member pricing on Canadian passport photos",
+      "Member pricing on merchandise online with shopwithcaa.com and in CAA Stores⁴",
+      "Save instantly at any of the 124,000 participating retail locations in North America. You can save on apparel, restaurants, the latest tech, entertainment and more",
+      "Save or get perks on travel, including car rentals, hotel stays, vacation packages and more"
+    ],
+    "benefits": [
+      "Unlimited general practitioner consults per year at a 20% discounted rate with Maple",
+      "Road Trip Accident Assistance⁵",
+      "",
+      ""
+    ]
   },
   {
     "cardName": "Plus",
     "cardValue": 111,
     "cardImg": "https://www.caasco.com/~/media/caasco/membership/cards/membership_card_Plus.png",
-    "cardLink": "https://www.caasco.com/membership/Join?Type=Plus"
+    "cardLink": "https://www.caasco.com/membership/Join?Type=Plus",
+    "roadside": [
+      "Four service calls",
+      "Towing distance of 200 km per call¹",
+      "Free emergency fuel and delivery.",
+      "Mobile battery service²",
+      "Member pricing on CAA Premium Battery purchase²",
+      "CAA Bike Assist™³",
+      "Car, bike and motorcycle coverage"
+    ],
+    "savings": [
+      "Free Canadian passport photos",
+      "Additional 5% off Member pricing on merchandise online with shopwithcaa.com and in CAA Stores⁴",
+      "Save instantly at any of the 124,000 participating retail locations in North America. You can save on apparel, restaurants, the latest tech, entertainment and more",
+      "Save or get perks on travel, including car rentals, hotel stays, vacation packages and more"
+    ],
+    "benefits": [
+      "2 free general practitioner consults per year with Maple",
+      "Road Trip Accident Assistance⁵",
+      "",
+      ""
+    ]
   },
   {
     "cardName": "Premier",
     "cardValue": 222,
     "cardImg": "https://www.caasco.com/~/media/caasco/membership/cards/membership_card_Premier.png",
-    "cardLink": "https://www.caasco.com/membership/Join?Type=Premier"
+    "cardLink": "https://www.caasco.com/membership/Join?Type=Premier",
+    "roadside": [
+      "Five service calls",
+      "Towing distance of 320 km on one call and 200 km on four calls¹",
+      "Free emergency fuel and delivery.",
+      "Mobile battery service²",
+      "Member pricing on CAA Premium Battery purchase plus 20 CAA Dollars®²",
+      "CAA Bike Assist™³",
+      "Car, bike and motorcycle coverage"
+    ],
+    "savings": [
+      "Free Canadian passport photos",
+      "Additional 10% off Member pricing on merchandise online with shopwithcaa.com and in CAA Stores⁴",
+      "Save instantly at any of the 124,000 participating retail locations in North America. You can save on apparel, restaurants, the latest tech, entertainment and more",
+      "Save or get perks on travel, including car rentals, hotel stays, vacation packages and more"
+    ],
+    "benefits": [
+      "2 free general practitioner consults per year with Maple",
+      "Road Trip Accident Assistance⁵",
+      "Road Trip Interruption Assistance⁶",
+      "Vehicle return benefit"
+    ]
   }
 ]
